d460dd8ce386b116bd6898be444ee7ac0c441748
Ayman Bouabra <aymanbouabra@gmail.com>
2025-02-13 15:29:49 +0100

    font familly and heading tools

--- app/documents/[documentId]/ToolBar.tsx
+++ app/documents/[documentId]/ToolBar.tsx
@@ -5,37 +5,108 @@ import { BoldIcon, ChevronDownIcon, ItalicIcon, ListTodoIcon, LucideIcon, Messag
 import React from 'react'
 import { useEditorStore } from "@/app/store/use-editor-store";
 import { Separator } from '@/components/ui/separator';
+import { type Level } from '@tiptap/extension-heading';
 
 import { DropdownMenu, DropdownMenuContent,DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 
-const FontFamilyButton = () => {
+const HeadingDropDown = () => {
+  const { editor } = useEditorStore();
+
+  const headings = [
+    {
+      label: "Normal text",
+      value: 0,
+      fontSize: "1rem",
+    },
+    {
+      label: "Heading 1",
+      value: 1,
+      fontSize: "2.25rem",
+    },
+    {
+      label: "Heading 2",
+      value: 2,
+      fontSize: "1.75rem",
+    },
+    {
+      label: "Heading 3",
+      value: 3,
+      fontSize: "1.5rem",
+    },
+    {
+      label: "Heading 4",
+      value: 4,
+      fontSize: "1.25rem",
+    },
+    {
+      label: "Heading 5",
+      value: 5,
+      fontSize: "1.1rem",
+    },
+    {
+      label: "Heading 6",
+      value: 6,
+      fontSize: "1rem",
+    },
+  ];
+
+  const getCurrentHeading = () => {
+    for(let level = 0; level < headings.length; level++) {
+      if(editor?.isActive("heading", { level })) {
+        return headings[level].label;
+      }
+    }
+    return headings[0].label;
+  }
+
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className={"h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"}>
+          <span className='truncate'>
+            {getCurrentHeading()}
+          </span> 
+          <ChevronDownIcon className='ml-2 size-4 shrink-0' />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='p-1 flex flex-col gap-y-1'>
+        {headings.map(({label, value, fontSize}) => (
+          <DropdownMenuItem 
+            key={value}
+            onClick={() => {
+              if(value === 0) {
+                editor?.chain().focus().setParagraph().run();
+              } else {
+                editor?.chain().focus().toggleHeading({ level: value as Level}).run();
+              }
+            }}
+            className={cn(
+              "flex items-center gap-x-2 px-2 py-3 rounded-sm hover:bg-neutral-200/80 hover:cursor-pointer",
+              (value === 0 && !editor?.isActive("heading")) || editor?.isActive("heading", { level: value }) && "bg-neutral-200/80"
+              )}
+            style={{fontSize}}
+          >
+            {label}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+const FontFamilyDropDown = () => {
   const { editor } = useEditorStore();
 
   const fonts = [
     { label: "Arial", value: "Arial" },
     { label: "Arial Black", value: "Arial Black" },
-    { label: "Calibri", value: "Calibri" },
-    { label: "Cambria", value: "Cambria" },
     { label: "Comic Sans MS", value: "Comic Sans MS" },
-    { label: "Consolas", value: "Consolas" },
-    { label: "Corsiva", value: "Corsiva" },
     { label: "Courier New", value: "Courier New" },
-    { label: "Georgia", value: "Georgia" },
     { label: "Impact", value: "Impact" },
-    { label: "Lora", value: "Lora" },
-    { label: "Merriweather", value: "Merriweather" },
-    { label: "Open Sans", value: "Open Sans" },
-    { label: "PT Sans", value: "PT Sans" },
-    { label: "PT Serif", value: "PT Serif" },
-    { label: "Roboto", value: "Roboto" },
-    { label: "Pacifico", value: "Pacifico" },
-    { label: "Source Sans Pro", value: "Source Sans Pro" },
-    { label: "Spectral", value: "Spectral" },
     { label: "Times New Roman", value: "Times New Roman" },
     { label: "Trebuchet MS", value: "Trebuchet MS" },
-    { label: "Ubuntu", value: "Ubuntu" },
     { label: "Verdana", value: "Verdana" },
-    { label: "Work Sans", value: "Work Sans" }
   ].sort((a, b) => a.label.localeCompare(b.label));
 
   return (
@@ -44,23 +115,23 @@ const FontFamilyButton = () => {
         <button className={"h-7 w-[120px] shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"}>
           <span className='truncate'>
             {editor?.getAttributes("textStyle").fontFamily || "Arial"}
-          </span>
+          </span> 
           <ChevronDownIcon className='ml-2 size-4 shrink-0' />
         </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='p-1 flex flex-col gap-y-1'>
         {fonts.map(({label, value}) => (
-          <button 
+          <DropdownMenuItem 
             key={value}
             onClick={() => editor?.chain().focus().setFontFamily(value).run()}
             className={cn(
-              "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+              "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80 hover:cursor-pointer",
               editor?.getAttributes("textStyle").fontFamily === value && "bg-neutral-200/80"
               )}
             style={{fontFamily: value}}
           >
             <span className='text-sm'>{label}</span>
-          </button>
+          </DropdownMenuItem>
         ))}
       </DropdownMenuContent>
     </DropdownMenu>
@@ -173,9 +244,9 @@ const ToolBar = () => {
         ))
       }
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
-      <FontFamilyButton />
+      <FontFamilyDropDown />
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
-      {/* TODO: Heading */}
+      <HeadingDropDown />
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
       {/* TODO: Font size */}
       {
