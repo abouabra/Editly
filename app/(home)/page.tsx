fad876b3119a1a7b39a70e53149b2b0c5c04745b
Ayman Bouabra <aymanbouabra@gmail.com>
2025-02-21 22:47:02 +0100

    working on Seach Input

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import NavBar from "./navbar";
 
 const Home = () => {
 	return (
-		<div className="w-screen h-screen flex items-center justify-center flex-col gap-4">
-			<Button>
+		<div className="min-h-screen flex flex-col">
+			<div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
+				<NavBar />
+			</div>
+			<div className="mt-16">
 				<Link href="/documents/123">Go To dummy document</Link>
-			</Button>
-			<div>Home</div>
+			</div>
 		</div>
 	);
 };
