3b87b154cf8a326e69dc143aaae3eb68ae269892
abouabra <aymanbouabra@gmail.com>
2025-03-02 16:47:00 +0000

    Templates Initial Content

--- app/(home)/templates-gallery.tsx
+++ app/(home)/templates-gallery.tsx
@@ -6,38 +6,9 @@ import { useRouter } from "next/navigation";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
+import { templates } from "@/constants/templates";
 
-const templates = [
-	{
-		id: "blank",
-		label: "Blank Document",
-		imageUrl: "/templates/blank-document.svg",
-	}, {
-    id: "software-proposal",
-    label: "Software Proposal",
-		imageUrl: "/templates/software-proposal.svg",
-  }, {
-    id: "project-proposal",
-    label: "Project Proposal",
-    imageUrl: "/templates/project-proposal.svg",
-  }, {
-    id: "business-letter",
-    label: "Business Letter",
-    imageUrl: "/templates/business-letter.svg",
-  }, {
-    id: "resume",
-    label: "Resume",
-    imageUrl: "/templates/resume.svg",
-  }, {
-    id: "cover-letter",
-    label: "Cover Letter",
-    imageUrl: "/templates/cover-letter.svg",
-  }, {
-    id: "letter",
-    label: "Letter",
-    imageUrl: "/templates/letter.svg",
-  }
-];
+
 
 const TemplatesGallery = () => {
 	const router = useRouter();
@@ -71,7 +42,7 @@ const TemplatesGallery = () => {
 									<button
 										disabled={isCreating}
 										// TODO: Proper Initial Content
-										onClick={() => onTemplateClick(template.label, "")}
+										onClick={() => onTemplateClick(template.label, template.initialContent)}
 										style={{
 											backgroundImage: `url(${template.imageUrl})`,
 											backgroundSize: "cover",
--- app/documents/[documentId]/document.tsx
+++ app/documents/[documentId]/document.tsx
@@ -23,7 +23,7 @@ const Document = ({ preloadedDocument }: DocumentIdProps) => {
 					<ToolBar />
 				</div>
 				<div className="pt-[114px] print:pt-0">
-					<Editor />
+					<Editor initialContent={document.initialContent} />
 				</div>
 			</div>
 		</Room>
